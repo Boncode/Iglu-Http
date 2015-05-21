8b75e783ca21a868b54ae04239eb8e0bc63b838d
Jeroen Arnoldus <jeroen@repleo.nl>
2015-05-21 15:48:25 +0200

    Test JSON fixed

--- src/test/java/org/ijsberg/iglu/http/json/JsonHierarchicalPropertiesObjectTest.java
+++ src/test/java/org/ijsberg/iglu/http/json/JsonHierarchicalPropertiesObjectTest.java
@@ -12,23 +12,25 @@ public class JsonHierarchicalPropertiesObjectTest {
 	private static Properties testProperties = new Properties();
 	static {
 		testProperties.setProperty("main", "bogus");
+        testProperties.setProperty("sub.bar", "hop");
 		testProperties.setProperty("sub.foo", "bla");
-		testProperties.setProperty("sub.bar", "hop");
+        testProperties.setProperty("sub.sub1.bar", "hop1");
 		testProperties.setProperty("sub.sub1.foo", "bla1");
-		testProperties.setProperty("sub.sub1.bar", "hop1");
+
 	}
 
 	private static String result = "{\n" +
 			"  \"main\" : \"bogus\", \"sub\" : {\n" +
-			"  \"foo\" : \"bla\", \"bar\" : \"hop\", \"sub1\" : {\n" +
-			"  \"foo\" : \"bla1\", \"bar\" : \"hop1\" }\n" +
+			"  \"bar\" : \"hop\", \"foo\" : \"bla\", \"sub1\" : {\n" +
+			"  \"bar\" : \"hop1\", \"foo\" : \"bla1\" }\n" +
 			" }\n" +
 			" }\n";
 
 	@Test
 	public void testConstructor() throws Exception {
 		JsonHierarchicalPropertiesObject object = new JsonHierarchicalPropertiesObject(testProperties);
-
+        // better is to unparse the generated json-object and parse it again, and assert original object with the reparsed object.
+        // JSON is not order-dependent, while the string comparison may fail on order changes.
 		Assert.assertEquals(result, object.toString());
 	}
 
