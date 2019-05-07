104159dea9677044b343d3cf9a675520654546f1
Jeroen Meetsma <jeroen@ijsberg.nl>
2019-05-07 13:42:16 +0200

    bugfix annotations, fix engineering hamburger, implemented dashboard getByName

--- src/main/resources/admin/www/lib/widget.js
+++ src/main/resources/admin/www/lib/widget.js
@@ -47,7 +47,7 @@ Widget.prototype.set = function(name, value, defaultValue) {
 	} else if(typeof defaultValue != 'undefined') {
 		this[name] = defaultValue;
 	}
-	console.log(name + ' set to ' + value + ' : ' + this[name]);
+	//console.log(name + ' set to ' + value + ' : ' + this[name]);
 };
 
 Widget.prototype.cloneSettings = function(extraSettingsNames) {
