9edfff95d9439ab4f646616c6f89a177a062bd4b
Jeroen Meetsma <jeroen@ijsberg.nl>
2020-02-20 16:02:42 +0100

    flex transition: aligned dashboard resources with modules and fixed ChartJS sizing by HACK

--- src/main/resources/admin/www/lib/flexpanelwidget.js
+++ src/main/resources/admin/www/lib/flexpanelwidget.js
@@ -111,14 +111,14 @@ FlexPanelWidget.prototype.writeHTML = function() {
         this.setHeaderContent();
 	}
 
-	var contentFrame = new FrameWidget({
+	var contentFrame = new FlexFrameWidget({
         id : this.id + '_frame',
         cssClassName : 'flexpanelcontentframe',
         //todo margin
-        top: (this.hasHeader ? 45 : 0),
+       /* top: (this.hasHeader ? 45 : 0),
         left: 0,
         width: (this.width),
-        height: (this.hasHeader ? (this.height - 45) : (this.height - 0))
+        height: (this.hasHeader ? (this.height - 45) : (this.height - 0))*/
 	}, this.content);
 /*	contentFrame.stretchToOuterWidget(this, {'e':{'offset':0}});
 	contentFrame.stretchToOuterWidget(this, {'s':{'offset':0}});
