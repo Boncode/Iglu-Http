63d0a312653f14c82870204b90eb75858e554f03
Koen Naarding <koen_naarding97@hotmail.com>
2020-02-21 14:14:17 +0100

    added loadTextData() to engineering dashboard iglu.util.import for loading .html etc. into vars
    removed dependency table and best practices from menu, are now
    configurable in dashboard templates in analysis.properties.
    See template.analysis.properties for example.
    
    Added json dashboards for the default FPA, best practices and dependency
    overviews in engineering dashboard.
    
    The overviews are now displayed in the header of the dashboard in a
    tabbed manner.
    
    Loading metrics now happens before displayMetricDetails is executed.
    
    Based on moduleDepth and which dashboard you want to load (FPA / best
    practices etc), displayMetricDetails will remember the dashboard type
    for consistency.
    
    Best practices / dependency / FPA table will be loaded with an onLoad
    function in a WebContentWidget.
    
    ----------------------------------------
    webcontent widget is smarter; can load onloads

--- src/main/resources/admin/www/lib/iglu.util.import.js
+++ src/main/resources/admin/www/lib/iglu.util.import.js
@@ -4,11 +4,13 @@ iglu.util.import.loadJsonData = function(data, servletPath, callbackWhenDone, ca
 	iglu.util.import.callbackWhenFilesLoaded = callbackWhenDone;
 	iglu.util.import.callbackInput = callbackInput;
 	iglu.util.import.nrofFilesToLoad = data.length;
+	console.log(data);
 	for(var i in data) {
 	    try {
 		    ajaxRequestManager.doRequest((typeof servletPath != 'undefined' ? servletPath : './') + data[i][1], new Function("jsonData", iglu.util.import.getJsonParseFunctionText(data[i][0])));
         } catch(e) {
             console.error('unable to perform request ' + servletPath + ': ' + e.message);
+            console.log(data[i][0]);
         }
 	}
 }
@@ -27,4 +29,21 @@ iglu.util.import.checkNrofFilesToLoad = function() {
 	if(iglu.util.import.nrofFilesToLoad == 0 && typeof iglu.util.import.callbackWhenFilesLoaded != 'undefined') {
     	iglu.util.import.callbackWhenFilesLoaded.call(this, iglu.util.import.callbackInput);
 	}
+}
+
+
+iglu.util.import.loadTextData = function(data, servletPath, callbackWhenDone, callbackInput) {
+	console.log(data);
+	for(var i in data) {
+	    try {
+		    ajaxRequestManager.doRequest((typeof servletPath != 'undefined' ? servletPath : './') + data[i][1], new Function("jsonData", iglu.util.import.handleLoadTextFile(data[i][0])));
+        } catch(e) {
+            console.error('unable to perform request ' + servletPath + ': ' + e.message);
+            console.log(data[i][0]);
+        }
+	}
+}
+
+iglu.util.import.handleLoadTextFile = function(varName) {
+    return "" + varName + " = jsonData";
 }
