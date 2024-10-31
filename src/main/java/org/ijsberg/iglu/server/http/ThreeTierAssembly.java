6eb18be9af0bb92beac7e458c139257350b9395e
Jeroen Meetsma <jeroen@ijsberg.nl>
2024-10-31 09:48:28 +0100

    assembly structure simplified

--- src/main/java/org/ijsberg/iglu/server/http/ThreeTierAssembly.java
+++ src/main/java/org/ijsberg/iglu/server/http/ThreeTierAssembly.java
@@ -5,15 +5,22 @@ import org.ijsberg.iglu.access.asset.AssetAccessManager;
 import org.ijsberg.iglu.access.asset.StandardAssetAccessManager;
 import org.ijsberg.iglu.access.component.RequestRegistry;
 import org.ijsberg.iglu.access.component.StandardAccessManager;
+import org.ijsberg.iglu.configuration.Assembly;
 import org.ijsberg.iglu.configuration.Cluster;
 import org.ijsberg.iglu.configuration.Component;
 import org.ijsberg.iglu.configuration.module.BasicAssembly;
 import org.ijsberg.iglu.configuration.module.StandardComponent;
 import org.ijsberg.iglu.event.ServiceBroker;
 import org.ijsberg.iglu.event.module.BasicServiceBroker;
+import org.ijsberg.iglu.invocation.RootConsole;
+import org.ijsberg.iglu.logging.module.RotatingFileLogger;
+import org.ijsberg.iglu.logging.module.StandardOutLogger;
 import org.ijsberg.iglu.scheduling.module.StandardScheduler;
 import org.ijsberg.iglu.usermanagement.multitenancy.component.MultiTenantAwareComponent;
+import org.ijsberg.iglu.util.properties.IgluProperties;
 
+import java.io.IOException;
+import java.util.Map;
 import java.util.Properties;
 
 /**
@@ -21,8 +28,18 @@ import java.util.Properties;
  */
 public abstract class ThreeTierAssembly extends BasicAssembly {
 
-    protected Component accessManager;
+    public static final String LOGGER = "Logger";
+    public static final String SCHEDULER = "Scheduler";
+    public static final String ACCESS_MANAGER = "AccessManager";
+    public static final String SERVICE_BROKER = "ServiceBroker";
+    public static final String ASSET_ACCESS_MANAGER = "AssetAccessManager";
+    public static final String ROOT_CONSOLE = "RootConsole";
+
+    protected Component logger;
+    protected Component rootConsole;
+
     protected Component scheduler;
+    protected Component accessManager;
     protected Component serviceBroker;
     protected Component assetAccessManager;
 
@@ -52,7 +69,7 @@ public abstract class ThreeTierAssembly extends BasicAssembly {
         createLayers(properties);
     }
 */
-    public ThreeTierAssembly(Properties properties, Component[] providedComponents) {
+/*    public ThreeTierAssembly(Properties properties, Component[] providedComponents) {
         super(properties);
         this.accessManager = providedComponents[0];
         this.scheduler = providedComponents[1];
@@ -62,6 +79,18 @@ public abstract class ThreeTierAssembly extends BasicAssembly {
         if(providedComponents.length > 3) {
             this.assetAccessManager = providedComponents[3];
         }
+        createLayers(properties);
+    }*/
+
+    public ThreeTierAssembly(Properties properties, Assembly coreAssembly) {
+        super(properties);
+        Map<String,Component> coreClusterComponents = coreAssembly.getCoreCluster().getInternalComponents();
+        logger = coreClusterComponents.get(LOGGER);
+        rootConsole = coreClusterComponents.get(ROOT_CONSOLE);
+        scheduler = coreClusterComponents.get(SCHEDULER);
+        accessManager = coreClusterComponents.get(ACCESS_MANAGER);
+        serviceBroker = coreClusterComponents.get(SERVICE_BROKER);
+        assetAccessManager = coreClusterComponents.get(ASSET_ACCESS_MANAGER);
         createLayers(properties);
     }
 
@@ -88,34 +117,71 @@ public abstract class ThreeTierAssembly extends BasicAssembly {
 
     protected Cluster createInfraLayer() {
 
-        //super.createInfraLayer();
+        if(logger == null) {
+            createLoggerComponent();
+        }
+        core.connect("Logger", logger);
+
+        if(rootConsole == null) {
+            rootConsole = new StandardComponent(new RootConsole(this));
+            core.connect(ROOT_CONSOLE, rootConsole);
+        }
+
         if(scheduler == null) {
             scheduler = new StandardComponent(new StandardScheduler());
         }
-        core.connect("Scheduler", scheduler);
+        core.connect(SCHEDULER, scheduler);
 
         if(accessManager == null) {
-            StandardAccessManager standardAccessManager = new StandardAccessManager(MultiTenantAwareComponent.class);
-            Properties accessManProps = new Properties();
-            accessManProps.setProperty("session_timeout", "" + Integer.parseInt(properties.getProperty("sessionTimeout", "60")));
-            accessManProps.setProperty("session_timeout_logged_in", "" + Integer.parseInt(properties.getProperty("sessionTimeoutLoggedIn", "1800")));
-//            accessManProps.setProperty("session_timeout", "" + (60 * 60 * 24));
-            standardAccessManager.setProperties(accessManProps);
-            accessManager = new StandardComponent(standardAccessManager);
+            creatAccessManagerComponent();
         }
-        core.connect("AccessManager", accessManager, RequestRegistry.class, AccessManager.class);
+        core.connect(ACCESS_MANAGER, accessManager, RequestRegistry.class, AccessManager.class);
         core.connect("RequestRegistry", accessManager, RequestRegistry.class);
 
         if(serviceBroker == null) {
             serviceBroker = new StandardComponent(new BasicServiceBroker());
         }
-        core.connect("ServiceBroker", serviceBroker, ServiceBroker.class);
+        core.connect(SERVICE_BROKER, serviceBroker, ServiceBroker.class);
 
         if(assetAccessManager == null) {
             assetAccessManager = new StandardComponent(new StandardAssetAccessManager());
         }
-        core.connect("AssetAccessManager", assetAccessManager, AssetAccessManager.class);
+        core.connect(ASSET_ACCESS_MANAGER, assetAccessManager, AssetAccessManager.class);
 
         return core;
     }
+
+    private void creatAccessManagerComponent() {
+        StandardAccessManager standardAccessManager = new StandardAccessManager(MultiTenantAwareComponent.class);
+        Properties accessManProps = new Properties();
+        accessManProps.setProperty("session_timeout", "" + Integer.parseInt(properties.getProperty("sessionTimeout", "60")));
+        accessManProps.setProperty("session_timeout_logged_in", "" + Integer.parseInt(properties.getProperty("sessionTimeoutLoggedIn", "1800")));
+        standardAccessManager.setProperties(accessManProps);
+        accessManager = new StandardComponent(standardAccessManager);
+    }
+
+    private void createLoggerComponent() {
+        RotatingFileLogger rotatingFileLogger = new RotatingFileLogger("logs/" + this.getClass().getSimpleName());
+        logger = new StandardComponent(rotatingFileLogger);
+        Properties loggerProperties;
+        if (IgluProperties.propertiesExist(home + "/" + configDir + "/logger.properties")) {
+            loggerProperties = IgluProperties.loadProperties(home + "/" + configDir + "/logger.properties");
+        } else {
+            loggerProperties = new IgluProperties();
+            loggerProperties.setProperty("log_level", "DEBUG");
+            loggerProperties.setProperty("log_to_standard_out", "true");
+            try {
+                IgluProperties.saveProperties(loggerProperties, home + "/" + configDir + "/logger.properties");
+            } catch (IOException e) {
+                System.err.println("could not save logger.properties with message: " + e.getMessage());
+            }
+        }
+        logger.setProperties(loggerProperties);
+
+        if (Boolean.parseBoolean(loggerProperties.getProperty("log_to_standard_out", "false"))) {
+            rotatingFileLogger.addAppender(new StandardOutLogger());
+        }
+    }
+
+
 }
