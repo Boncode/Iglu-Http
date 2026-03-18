b09f59b6c23c87618c3c91a72411682808ac7306
Jeroen Meetsma <j.meetsma@boncode.nl>
2026-03-18 13:56:54 +0100

    Working on intrusion detection. Monitor details for server are fetched directly instead of cached for 5 minutes. Possible fix for linux shell commands for monitoring not giving responses sometimes.

--- src/main/java/org/ijsberg/iglu/server/http/filter/WebTrafficFilter.java
+++ src/main/java/org/ijsberg/iglu/server/http/filter/WebTrafficFilter.java
@@ -19,10 +19,16 @@ public class WebTrafficFilter implements Filter {
     @Override
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
 
+        if(!webTrafficMonitor.allowRequest((HttpServletRequest)servletRequest, (HttpServletResponse)servletResponse)) {
+            System.out.println("Web traffic not allowed");
+            ((HttpServletResponse) servletResponse).sendError(418);
+            return;
+        }
+
         String pathInfo = ((HttpServletRequest) servletRequest).getPathInfo();
         filterChain.doFilter(servletRequest, servletResponse);
         if(!"/messages/latest".equals(pathInfo)) {
-            System.out.println(new LogEntry(((HttpServletRequest)servletRequest).getPathInfo() + " " + ((HttpServletResponse)servletResponse).getStatus()));
+            //System.out.println(new LogEntry(((HttpServletRequest)servletRequest).getPathInfo() + " " + ((HttpServletResponse)servletResponse).getStatus()));
             webTrafficMonitor.allowResponse((HttpServletRequest)servletRequest, (HttpServletResponse)servletResponse);
         }
     }
--- src/main/java/org/ijsberg/iglu/server/http/module/WebTrafficMonitorImpl.java
+++ src/main/java/org/ijsberg/iglu/server/http/module/WebTrafficMonitorImpl.java
@@ -2,16 +2,29 @@ package org.ijsberg.iglu.server.http.module;
 
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.ijsberg.iglu.configuration.Startable;
 import org.ijsberg.iglu.event.EventBus;
-import org.ijsberg.iglu.server.http.WebTrafficEvent;
+import org.ijsberg.iglu.scheduling.Pageable;
 import org.ijsberg.iglu.server.http.WebTrafficMonitor;
+import org.ijsberg.iglu.util.dataanalysis.OccurrenceRecord;
+import org.ijsberg.iglu.util.time.TimePeriod;
+import org.ijsberg.iglu.util.time.TimeUnit;
 
-import static org.ijsberg.iglu.server.http.WebTrafficEvent.WebTrafficEventType.SUSPECTED_HACKING_ATTEMPT;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
 
-public class WebTrafficMonitorImpl implements WebTrafficMonitor {
+import static org.ijsberg.iglu.util.http.HttpSupport.getClientIpAddress;
+
+public class WebTrafficMonitorImpl implements WebTrafficMonitor, Startable, Pageable {
+
+    //TODO trustedIps
 
     private String assetId;
     private EventBus eventBus;
+    private boolean isStarted;
+    private HashSet<String> suspicousIps = new HashSet<>();
+    private Map<String, OccurrenceRecord> accessDeniedRecordsByIp = new HashMap<>();
 
     public WebTrafficMonitorImpl(String assetId) {
         this.assetId = assetId;
@@ -25,15 +38,73 @@ public class WebTrafficMonitorImpl implements WebTrafficMonitor {
 
     @Override
     public boolean allowRequest(HttpServletRequest req, HttpServletResponse resp) {
-        return true;
+        String clientIpAddress = getClientIpAddress(req);
+        return !suspicousIps.contains(clientIpAddress);
     }
 
     @Override
     public boolean allowResponse(HttpServletRequest req, HttpServletResponse resp) {
-        if(!testMessageSent) {
-            testMessageSent = true;
-            eventBus.publish(new WebTrafficEvent(SUSPECTED_HACKING_ATTEMPT, assetId, "this is a test message from " + WebTrafficMonitorImpl.class.getSimpleName()));
+        int status = resp.getStatus();
+        if(status != 200) {
+            if(!isDevToolRequest(req)) {
+                String clientIpAddress = getClientIpAddress(req);
+                OccurrenceRecord record = getAccessDeniedRecord(clientIpAddress);
+                record.recordOccurrence(req.getPathInfo());
+                System.out.println("--> " + req.getPathInfo() + ":" + status + " : " + record.getNrOccurrences());
+                if(!record.isNrOccurrencesBelow(5, new TimePeriod(5, TimeUnit.SECOND))) {
+                    System.out.println("=======================================================================");
+                    System.out.println(" SUSPECTED HACKING ATTEMPT " + clientIpAddress);
+                    System.out.println("=======================================================================");
+                    suspicousIps.add(clientIpAddress);
+//                  eventBus.publish(new WebTrafficEvent(SUSPECTED_HACKING_ATTEMPT, assetId, "this is a test message from " + WebTrafficMonitorImpl.class.getSimpleName() + ", IP: " + clientIpAddress));
+                }
+            }
         }
         return true;
+    }
+
+    private boolean isDevToolRequest(HttpServletRequest req) {
+        String path = req.getPathInfo();
+        return path.endsWith("devtools.json") || path.endsWith(".map");
+    }
+
+    private OccurrenceRecord getAccessDeniedRecord(String ip) {
+        OccurrenceRecord record = accessDeniedRecordsByIp.get(ip);
+        if(record == null) {
+            record = new OccurrenceRecord();
+            accessDeniedRecordsByIp.put(ip, record);
+        }
+        return record;
+    }
+
+    @Override
+    public void start() {
+        isStarted = true;
+    }
+
+    @Override
+    public int getPageIntervalInMinutes() {
+        return 2;
+    }
+
+    @Override
+    public int getPageOffsetInMinutes() {
+        return 0;
+    }
+
+    @Override
+    public void onPageEvent(long officialTime) {
+        //cleanup
+        suspicousIps.clear();
+    }
+
+    @Override
+    public boolean isStarted() {
+        return isStarted;
+    }
+
+    @Override
+    public void stop() {
+        isStarted = false;
     }
 }
